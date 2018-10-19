620569f0c1beeb0f4b7622c96d30efdc2b86a2d4
wolfyr <35544104+wolfyr@users.noreply.github.com>
2018-10-19 17:32:13 +0200

    image slideshow

--- my-app/src/app/components/gallary/gallary.component.ts
+++ my-app/src/app/components/gallary/gallary.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {TweenMax, Power2, TimelineLite} from "gsap";
+
 //Imported models
 import { ArtCollection } from '../../models/ArtCollection';
 
@@ -15,6 +17,7 @@ import { ArtCollectionService } from './../../services/art-collection.service';
 })
 export class GallaryComponent implements OnInit {
 
+  activeImg: string;
   displayedImg: HTMLElement;
   collectionsHolder: HTMLElement;
   artCollections: Array<ArtCollection>;
@@ -22,45 +25,59 @@ export class GallaryComponent implements OnInit {
   constructor(private artCollectionService: ArtCollectionService) {}
 
   ngOnInit() {
-    this.displayedImg = document.getElementById('displayedImg');
     this.collectionsHolder = document.getElementById('collections');
+    this.displayedImg = document.getElementById('displayedImg');
+    this.displayedImg.style.maxHeight = window.innerHeight / 2 + 'px';
     this.getArtCollections();
 
   }
+
 
   getArtCollections(): void {
     this.artCollectionService.getArtCollections()
       .subscribe(artCollections => {
         this.artCollections = artCollections
         console.log(artCollections, this.artCollections);
-
-        this.showCollectionsInDOM(this.artCollections);
-      });
-
-      
-      
+        this.slideShow('firstRun');
+      });   
   }
 
-  showCollectionsInDOM(artCollections){
-    console.log(artCollections);
-    this.displayedImg.setAttribute('src', artCollections[0].artCollection[0]);
-    this.startSlideShow();
 
-    for(let i = 0; i < artCollections.length; i++){
-      let collectionTitleElement = document.createElement('p');
-      collectionTitleElement.innerHTML = artCollections[i].title;
-      collectionTitleElement.setAttribute('class', 'collection');
-      if(i == 0){
-        collectionTitleElement.setAttribute('class', 'collection activeCollection');        
+  slideShow(e){
+    let collectionToUse;
+    let imgIndex = 0;
+    if(e === 'firstRun'){
+      collectionToUse = this.artCollections[0].artCollection;
+      this.activeImg = collectionToUse[imgIndex];
+    }else{
+      const clickedId = e.target.attributes['id'].value;
+      const idTextLength = clickedId.length - 1;
+      const index = parseInt(clickedId.charAt(idTextLength));
+      collectionToUse = this.artCollections[index].artCollection;
+      this.activeImg = collectionToUse[imgIndex];
+    }
+
+    const nextImg = () => {
+      if(imgIndex < collectionToUse.length - 1){
+        imgIndex++;
+      }else{
+        imgIndex = 0;
       }
-
-      this.collectionsHolder.appendChild(collectionTitleElement);
+      TweenMax.to(this.displayedImg, 1, {delay: 0.8, opacity: 1, onComplete: startAnimation});
+      setTimeout(() => {
+        this.displayedImg.setAttribute('src', collectionToUse[imgIndex])
+      }, 720);
+      
     }
     
+    const startAnimation = () => {
+      TweenMax.to(this.displayedImg, 1, {delay: 5, opacity: 0, onStart: nextImg});      
+    }
+
+    startAnimation()
+
   }
 
-  startSlideShow(){
-    console.log('Start image slide show');
-  }
+  
 
 }
